1331d0ff5d709b679805892747721f1cd1bebdb2
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-02 19:06:45 +0300

    Updates

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,45 +1,15 @@
-import { createTheme, NextUIProvider } from "@nextui-org/react";
-import "sf-font";
-import Link from "next/link";
-import {
-  Spacer,
-  Button,
-  Col,
-  Row,
-  Container,
-  Dropdown,
-} from "@nextui-org/react";
-import react from "react";
-
-const theme = createTheme({
-  type: "dark",
-  theme: {
-    fontFamily: "SF Pro Display",
-    colors: {
-      primaryLight: "$blue200",
-      primaryLightHover: "$blue300",
-      primaryLightActive: "$blue400",
-      primaryLightContrast: "$blue600",
-      primary: "$purple500",
-      primaryBorder: "$blue500",
-      primaryBorderHover: "$blue600",
-      primarySolidHover: "$blue700",
-      primarySolidContrast: "$white",
-      primaryShadow: "$white500",
-      transparent: "#00000000",
-
-      gradient:
-        "linear-gradient(112def, $blue100 -25%, $pink500 -10%, $purple300 90%)",
-      link: "#SE1DAD",
-      mycolor: "#00000030",
-    },
-    space: {},
-    fonts: {},
-  },
-});
+import Navbar from "../components/navbar";
+import { AlgoProvider } from "../provider/context";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AlgoProvider>
+      <Navbar>
+        <Component {...pageProps} />
+      </Navbar>
+    </AlgoProvider>
+  );
 }
 
 export default MyApp;
